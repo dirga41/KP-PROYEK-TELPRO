b2131cb73c0d5e00bc06e865672403e0687c1c91
dirga41 <dirgaeka336@gmail.com>
2025-07-07 14:29:46 +0700

    Update tab project plan
    Pembaruan nama update menjadi keterangan

--- resources/views/dashboards/project.blade.php
+++ resources/views/dashboards/project.blade.php
@@ -241,7 +241,7 @@
                                     <th class="py-3 px-6 text-left">User</th>
                                     <th class="py-3 px-6 text-left">Lokasi</th>
                                     <th class="py-3 px-6 text-left">Estimasi Nilai</th>
-                                    <th class="py-3 px-6 text-left">Update</th>
+                                    <th class="py-3 px-6 text-left">Keterangan</th>
                                     <th class="py-3 px-6 text-center">Actions</th>
                                 </tr>
                             </thead>
@@ -382,7 +382,7 @@
                     <div><label for="plan_user" class="block mb-2 text-sm font-medium text-gray-900">User</label><input type="text" name="user" id="plan_user" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required></div>
                     <div><label for="plan_lokasi" class="block mb-2 text-sm font-medium text-gray-900">Lokasi</label><input type="text" name="lokasi" id="plan_lokasi" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required></div>
                     <div><label for="plan_estimasi_nilai" class="block mb-2 text-sm font-medium text-gray-900">Estimasi Nilai</label><input type="number" name="estimasi_nilai" id="plan_estimasi_nilai" placeholder="Rp" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required></div>
-                    <div><label for="plan_update_info" class="block mb-2 text-sm font-medium text-gray-900">Update</label><textarea name="update_info" id="plan_update_info" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"></textarea></div>
+                    <div><label for="plan_update_info" class="block mb-2 text-sm font-medium text-gray-900">Keterangan</label><textarea name="update_info" id="plan_update_info" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"></textarea></div>
                 </div>
                 <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b justify-end"><button id="cancelPlanInputModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10">Cancel</button><button type="submit" class="text-white bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Simpan</button></div>
             </form>
