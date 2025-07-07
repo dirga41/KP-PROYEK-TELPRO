ac5ec4c4a4936b274c1f37abdc45d27d00d75f9a
RONIASHARI <roniashari1234@gmail.com>
2025-07-07 10:25:39 +0700

    lanjut

--- resources/views/dashboards/project.blade.php
+++ resources/views/dashboards/project.blade.php
@@ -48,7 +48,7 @@
             <input id="searchInput" type="text" class="w-full pl-10 pr-4 py-2 border rounded-lg bg-gray-100 focus:outline-none focus:ring-2 focus:ring-red-500" placeholder="Search for...">
         </div>
 
-        <!-- Navigation -->
+        <!-- Nav -->
         <nav id="mainNav" class="flex-grow">
             <ul>
                 <li class="nav-item mb-2">
